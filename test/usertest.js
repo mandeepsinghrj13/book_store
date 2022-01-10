e6787ebf242a5d6485d53aec7ea93ba15be2f6e7
mandeep <singh.mr.1996@gmail.com>
2022-01-10 21:40:56 +0530

    Refacter: refacter package-lock file and  testcase file

--- test/usertest.js
+++ test/usertest.js
@@ -7,26 +7,26 @@ const registrationData = require("./user.json");
 chai.should();
 
 describe("user registartion", () => {
-  //   it("givenRegistrationDetails_whenProper_shouldSaveInDB", (done) => {
-  //     const registerfaker = {
-  //       firstName: "userFirst",
-  //       lastName: "userLast",
-  //       email: "newemail@gmail.com",
-  //       password: "Password@123",
-  //       role: "user",
-  //     };
-  //     chai
-  //       .request(server)
-  //       .post("/userRegistration")
-  //       .send(registerfaker)
-  //       .end((err, res) => {
-  //         if (err) {
-  //           return done(err);
-  //         }
-  //         res.should.have.status(200);
-  //         done();
-  //       });
-  //   });
+  it("givenRegistrationDetails_whenProper_shouldSaveInDB", (done) => {
+    const registerfaker = {
+      firstName: "userFirst",
+      lastName: "userLast",
+      email: "newuser007@gmail.com",
+      password: "Password@123",
+      role: "user",
+    };
+    chai
+      .request(server)
+      .post("/userRegistration")
+      .send(registerfaker)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        res.should.have.status(200);
+        done();
+      });
+  });
   it("givenRegistrationDetails_whenNoProper_NotSaveInDB", (done) => {
     chai
       .request(server)
@@ -40,23 +40,23 @@ describe("user registartion", () => {
 });
 
 describe("admin registartion", () => {
-  //   it("givenRegistrationDetails_whenProper_shouldSaveInDB", (done) => {
-  //     const registerfaker = {
-  //       firstName: "adminFirst",
-  //       lastName: "adminLast",
-  //       email: "newadminemail@gmail.com",
-  //       password: "Password@123",
-  //       role: "admin",
-  //     };
-  //     chai
-  //       .request(server)
-  //       .post("/adminRegistration")
-  //       .send(registerfaker)
-  //       .end((err, res) => {
-  //         res.should.have.status(200);
-  //         done();
-  //       });
-  //   });
+  it("givenRegistrationDetails_whenProper_shouldSaveInDB", (done) => {
+    const registerfaker = {
+      firstName: "adminFirst",
+      lastName: "adminLast",
+      email: "newadmin008@gmail.com",
+      password: "Password@123",
+      role: "admin",
+    };
+    chai
+      .request(server)
+      .post("/adminRegistration")
+      .send(registerfaker)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
   it("givenAdminRegistrationDetails_whenNotProper_thenNotSaveInDB", (done) => {
     chai
       .request(server)
