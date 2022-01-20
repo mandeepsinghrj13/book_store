c6154305f61b1dad7efd6d72c07e042f8072fba0
mandeep <singh.mr.1996@gmail.com>
2022-01-20 18:58:06 +0530

    Feat: add testcase for remove book from cart

--- test/carttest.js
+++ test/carttest.js
@@ -153,3 +153,53 @@ describe("placeOrder api", () => {
       });
   });
 });
+
+describe("removeBookFromCart api", () => {
+  it("givenValidBook_ValidTokenAndQuantityShould_AddToCart", (done) => {
+    const token = data.validToken;
+    const qty = {
+      qty: 2,
+    };
+    chai
+      .request(server)
+      .post("/addToCart/61e2ede9c3e5c7f047590d32")
+      .set({ authorization: token })
+      .send(qty)
+      .end((err, res) => {
+        res.should.have.status(201);
+        done();
+      });
+  });
+
+  it("givenValidToken_ShouldBe_removeBookFromCart", (done) => {
+    const token = data.validToken;
+    const removeBookId = {
+      bookId: "61e2ede9c3e5c7f047590d32",
+    };
+    chai
+      .request(server)
+      .delete("/removeBookFromCart")
+      .set({ authorization: token })
+      .send(removeBookId)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it("givenInvalidToken_ShouldNot_removeBookFromCart", (done) => {
+    const token = data.invalidToken;
+    const removeBookId = {
+      bookId: "61e2ede9c3e5c7f047590d32",
+    };
+    chai
+      .request(server)
+      .delete("/removeBookFromCart")
+      .set({ authorization: token })
+      .send(removeBookId)
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+});
