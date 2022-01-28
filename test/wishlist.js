dd94dbb56fbeac0b7a3065a5f7257de61cd46960
mandeep <singh.mr.1996@gmail.com>
2022-01-28 20:34:43 +0530

    Feat: add testcase for wishlist

---
+++ test/wishlist.js
@@ -0,0 +1,73 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../server");
+
+chai.use(chaiHttp);
+
+chai.should();
+const token =
+  "b eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhRm9yVG9rZW4iOnsiaWQiOiI2MWVlMmQwMDcyMWQ1NzY5NTViMDQ2YzYiLCJmaXJzdE5hbWUiOiJhZG1pbiIsImxhc3ROYW1lIjoiYWRtaW4iLCJlbWFpbCI6Im5ld3VzZXJhZG1pbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNjQyOTk5MjYyfQ._PBVzGYcSyA7NHocbF50F3almIsi7gOaf_dmSji4KNE";
+const Invalidtoken =
+  "b yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhRm9yVG9rZW4iOnsiaWQiOiI2MWVlMmQwMDcyMWQ1NzY5NTViMDQ2YzYiLCJmaXJzdE5hbWUiOiJhZG1pbiIsImxhc3ROYW1lIjoiYWRtaW4iLCJlbWFpbCI6Im5ld3VzZXJhZG1pbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNjQyOTk5MjYyfQ._PBVzGYcSyA7NHocbF50F3almIsi7gOaf_dmSji4KNE";
+
+describe("AddToWishList api", () => {
+  it("givenValidBook_ValidTokenAndQuantityShould_AddToWishlist", (done) => {
+    const qty = {
+      qty: 2,
+    };
+    chai
+      .request(server)
+      .post("/wishlist/61e2ede9c3e5c7f047590d32")
+      .set({ authorization: token })
+      .send(qty)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it("givenValidBook_ValidTokenAndNegativeQuantityShouldReturn200(wishList updated)", (done) => {
+    const qty = {
+      qty: -1,
+    };
+    chai
+      .request(server)
+      .post("/wishlist/61e2ede9c3e5c7f047590d32")
+      .set({ authorization: token })
+      .send(qty)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it("givenValidBook_ValidTokenAndNegativeQuantityShouldReturn401TokenRequired", (done) => {
+    const token = "";
+    const qty = {
+      qty: -1,
+    };
+    chai
+      .request(server)
+      .post("/wishlist/61e2ede9c3e5c7f047590d32")
+      .set({ authorization: token })
+      .send(qty)
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it("givenValidBook_ValidTokenAndNegativeQuantityShouldReturn400InvalidToken", (done) => {
+    const qty = {
+      qty: -1,
+    };
+    chai
+      .request(server)
+      .post("/wishlist/61e2ede9c3e5c7f047590d32")
+      .set({ authorization: Invalidtoken })
+      .send(qty)
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+});
