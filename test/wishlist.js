0a0467dde7ad40ee9f3c5c97566bdefa23b78c9d
mandeep <singh.mr.1996@gmail.com>
2022-01-29 19:15:03 +0530

    Feat: add testcase for delete book into wishlist

--- test/wishlist.js
+++ test/wishlist.js
@@ -9,7 +9,7 @@ const token =
   "b eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhRm9yVG9rZW4iOnsiaWQiOiI2MWVlMmQwMDcyMWQ1NzY5NTViMDQ2YzYiLCJmaXJzdE5hbWUiOiJhZG1pbiIsImxhc3ROYW1lIjoiYWRtaW4iLCJlbWFpbCI6Im5ld3VzZXJhZG1pbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNjQyOTk5MjYyfQ._PBVzGYcSyA7NHocbF50F3almIsi7gOaf_dmSji4KNE";
 const Invalidtoken =
   "b yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhRm9yVG9rZW4iOnsiaWQiOiI2MWVlMmQwMDcyMWQ1NzY5NTViMDQ2YzYiLCJmaXJzdE5hbWUiOiJhZG1pbiIsImxhc3ROYW1lIjoiYWRtaW4iLCJlbWFpbCI6Im5ld3VzZXJhZG1pbkBnbWFpbC5jb20iLCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNjQyOTk5MjYyfQ._PBVzGYcSyA7NHocbF50F3almIsi7gOaf_dmSji4KNE";
-
+const bookId = "61e2ede9c3e5c7f047590d34";
 describe("AddToWishList api", () => {
   it("givenValidBook_ValidTokenAndQuantityShould_AddToWishlist", (done) => {
     const qty = {
@@ -65,6 +65,54 @@ describe("AddToWishList api", () => {
       .post("/wishlist/61e2ede9c3e5c7f047590d32")
       .set({ authorization: Invalidtoken })
       .send(qty)
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+});
+describe("Delete Book From Wishlist api", () => {
+  it("givenValidBook_ValidTokenAndShould_AddToWishList", (done) => {
+    console.log(bookId);
+    chai
+      .request(server)
+      .post(`/wishlist/${bookId}`)
+      .set({ authorization: token })
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it("givenValidToken_ShouldBe_removeBook_FromWishlist", (done) => {
+    console.log(bookId);
+    chai
+      .request(server)
+      .delete(`/wishlist/${bookId}`)
+      .set({ authorization: token })
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+
+  it("givenValidToken_ShouldBe_bookNotFound_Into_Wishlist", (done) => {
+    console.log(bookId);
+    chai
+      .request(server)
+      .delete(`/wishlist/${bookId}`)
+      .set({ authorization: token })
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+
+  it("givenInvalidToken_ShouldNot_removeBook", (done) => {
+    chai
+      .request(server)
+      .delete("/wishlist/61e2ede9c3e5c7f047590d34")
+      .set({ authorization: Invalidtoken })
       .end((err, res) => {
         res.should.have.status(400);
         done();
